af1127a547f5d560ce77700bb71960457fc6bbe7
Geziel Bananajoy <gezielmiau@gmail.com>
2023-05-11 14:22:41 -0300

    Criaçao dos metados

---
+++ Repositories/ITransactionRepositories.cs
@@ -0,0 +1,12 @@
+﻿using ControleFinanceiro.App.Models;
+
+namespace ControleFinanceiro.App.Repositories
+{
+    public interface ITransactionRepositories
+    {
+        void Add(Transaction transaction);
+        void Delete(Transaction transaction);
+        List<Transaction> GetAll();
+        void Update(Transaction transaction);
+    }
+}
---
+++ Repositories/TransactionRepository.cs
@@ -0,0 +1,50 @@
+﻿using ControleFinanceiro.App.Models;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.App.Repositories
+{
+    public class TransactionRepositories : ITransactionRepositories
+    {
+        
+        private readonly LiteDatabase _database;
+        private readonly string collectionName = ("transactions");
+        //Local do banco de dados
+        public TransactionRepositories(LiteDatabase database)
+        {
+            _database = database;
+        }
+        //Lista tranzasoes/buscar dados do banco
+        public List<Transaction> GetAll()
+        {
+            return _database
+                .GetCollection<Transaction>(collectionName)
+                .Query()
+                .OrderByDescending(a=>a.Date)
+                .ToList();
+        }
+        //Adisionar tranzasoes
+        public void Add(Transaction transaction)
+        {
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Insert(transaction);
+            col.EnsureIndex(a => a.Date);
+        }
+        //Atualizar tranzasoes
+        public void Update(Transaction transaction)
+        {
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Update(transaction);
+        }
+        //Deletar tranzasoes
+        public void Delete(Transaction transaction)
+        {
+            var col = _database.GetCollection<Transaction>(collectionName);
+            col.Delete(transaction.Id);
+        }
+    }
+}
