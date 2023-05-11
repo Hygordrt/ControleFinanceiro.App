31cfc437a54b1290bb781a9b7f1ab8eee4b4fe51
Geziel Bananajoy <gezielmiau@gmail.com>
2023-05-11 14:26:33 -0300

    Trocando tela inicial

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace ControleFinanceiro.App;
+﻿using ControleFinanceiro.App.Views;
+
+namespace ControleFinanceiro.App;
 
 public partial class App : Application
 {
@@ -6,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new TransactionList();
 	}
 }
