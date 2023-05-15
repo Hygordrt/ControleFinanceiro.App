4cc45bd02eb744e91d041756ce9b396e7a7cb7be
Hygor Divino Ribeiro Teles <divinohygor@gmail.com>
2023-05-15 19:07:25 -0300

    - Correção no código XAML das telas de Add e Edit.

--- Views/TransactionEdit.xaml.cs
+++ Views/TransactionEdit.xaml.cs
@@ -7,7 +7,6 @@ public partial class TransactionEdit : ContentPage
         InitializeComponent();
     }
 
-
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
         Navigation.PopModalAsync();
